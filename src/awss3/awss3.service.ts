efb6b9fb3d9500ad72119cfae226c1208bf9c67c
Konstantin Baev <baev.exceedteam@gmail.com>
2022-02-16 18:48:36 +0300

    not so good

--- src/awss3/awss3.service.ts
+++ src/awss3/awss3.service.ts
@@ -1,10 +1,27 @@
-import { Injectable } from '@nestjs/common';
-import { InjectModel } from 'nestjs-objection/dist';
-import { S3Data } from './S3Data.model';
+import { Injectable, Logger } from '@nestjs/common';
+import { S3 } from 'aws-sdk';
 
 @Injectable()
 export class AWSS3Service {
-  constructor(@InjectModel(S3Data) s3Data) {}
-
-  
+  async uploadS3(file, bucket, name) {
+    const s3 = new S3({
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    });
+    const params = {
+      Bucket: bucket,
+      Key: String(name),
+      Body: file,
+    };
+    return new Promise<S3.ManagedUpload.SendData>((resolve, reject) => {
+      console.log(resolve);
+      s3.upload(params, (err, data) => {
+        if (err) {
+          Logger.error(err);
+          reject(err.message);
+        }
+        resolve(data);
+      });
+    });
+  }
 }
