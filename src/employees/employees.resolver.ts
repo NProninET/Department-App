d60b7415c34fe1018bfb7927428e2eb6f3aedcf8
Konstantin Baev <baev.exceedteam@gmail.com>
2022-02-16 11:43:16 +0300

    employees resolver little fixes

--- src/employees/employees.resolver.ts
+++ src/employees/employees.resolver.ts
@@ -4,38 +4,47 @@ import { Employee } from "./models/employees.model";
 import { EmployeeBase } from "./models/employees-base.model";
 import { CreateEmployeeInput } from "./inputs/create-employee.input";
 import { UpdateEmployeeInput } from "./inputs/update-employee.input";
+import { UploadedFile, UseInterceptors } from "@nestjs/common";
+import { FileInterceptor } from '@nestjs/platform-express';
+import { Express } from 'express';
 
 @Resolver(() => Employee)
 export class EmployeesResolver {
     constructor(private readonly employeesService: EmployeesService) {}
 
     @Mutation(() => EmployeeBase, {name: 'createEmployee'})
-    createEmployee(@Args('input') input: CreateEmployeeInput): Promise<EmployeeBase> {
-        return this.employeesService.createEmployee(input);
+    async createEmployee(@Args('input') input: CreateEmployeeInput): Promise<EmployeeBase> {
+        return await this.employeesService.createEmployee(input);
     }
 
     @Query(() => [Employee], {name: 'employees'})
-    findAll(): Promise<Employee[]> {
-        return this.employeesService.getAllEmployees();
+    async findAll(): Promise<Employee[]> {
+        return await this.employeesService.getAllEmployees();
     }
 
     @Query(() => [Employee], {name: 'employeesFromDepartment'})
-    getEmployeesFromDepartment(@Args('id') id: number): Promise<Employee[]> {
-        return this.employeesService.getAllEmployeesInDepartment(id);
+    async getEmployeesFromDepartment(@Args('id') id: number): Promise<Employee[]> {
+        return await this.employeesService.getAllEmployeesInDepartment(id);
     }
 
     @Query(() => Employee, {name: 'employee'})
-    findOne(@Args('id') id: number): Promise<Employee> {
-        return this.employeesService.getEmployeeById(id);
+    async findOne(@Args('id') id: number): Promise<Employee> {
+        return await this.employeesService.getEmployeeById(id);
     }
 
     @Mutation(() => Employee, {name: 'updateEmployee'})
-    updateEmployee(@Args('input') input: UpdateEmployeeInput): Promise<Employee> {
-        return this.employeesService.updateEmployee(input)
+    async updateEmployee(@Args('input') input: UpdateEmployeeInput): Promise<Employee> {
+        return await this.employeesService.updateEmployee(input)
     }
 
     @Mutation(() => Int, {name: 'removeEmployee'})
-    removeEmployee(@Args('id') id: number): Promise<number> {
-        return this.employeesService.removeEmployee(id);
+    async removeEmployee(@Args('id') id: number): Promise<number> {
+        return await this.employeesService.removeEmployee(id);
+    }
+
+    @Mutation(() => Employee, {name: 'uploadEmployeePhoto'})
+    @UseInterceptors(FileInterceptor('file'))
+    async uploadEmployeePhoto(@UploadedFile() file: Express.Multer.File): Promise<Employee> {
+        return await this.employeesService.uploadEmployeePhoto(file);
     }
 }
